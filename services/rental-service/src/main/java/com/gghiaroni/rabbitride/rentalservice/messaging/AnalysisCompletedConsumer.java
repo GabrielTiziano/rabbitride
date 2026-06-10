16d3e9e56a195f09a26642b0cd545cbf3fcd2d24
GGhiaroni <gghiaronitiziano@gmail.com>
2026-06-10 14:27:40 -0300

    fix: Adiciona a annotation @Component no AnalysisCompletedConsumer e tira o ; do final do RabbitListener, que estava gerando erro

--- services/rental-service/src/main/java/com/gghiaroni/rabbitride/rentalservice/messaging/AnalysisCompletedConsumer.java
+++ services/rental-service/src/main/java/com/gghiaroni/rabbitride/rentalservice/messaging/AnalysisCompletedConsumer.java
@@ -6,7 +6,9 @@ import com.gghiaroni.rabbitride.rentalservice.rental.RentalService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AnalysisCompletedConsumer {
     private static final Logger log = LoggerFactory.getLogger(AnalysisCompletedConsumer.class);
     private final RentalService rentalService;
@@ -15,7 +17,7 @@ public class AnalysisCompletedConsumer {
         this.rentalService = rentalService;
     }
 
-    @RabbitListener(queues = Queues.RENTAL_ANALYSIS_COMPLETED);
+    @RabbitListener(queues = Queues.RENTAL_ANALYSIS_COMPLETED)
     public void onAnalysisCompleted(AnalysisCompletedEvent event){
         log.info("Recebido AnalysisCompleted: rentalId={}, resultado={}", event.rentalId(), event.resultado());
         rentalService.processarResultadoAnalise(event);
